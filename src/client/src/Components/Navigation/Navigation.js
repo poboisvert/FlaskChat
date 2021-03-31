52fe0aa5211257639f879a14f33bb7b923a837e4
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-31 17:04:44 -0400

    Coins user

--- src/client/src/Components/Navigation/Navigation.js
+++ src/client/src/Components/Navigation/Navigation.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navigation.css";
 import FaceIcon from "@material-ui/icons/Face";
 import AddBoxIcon from "@material-ui/icons/AddBox";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Navigation() {
+  const [coins, setCoins] = useState(0);
+
+  useEffect(() => {
+    fetch("/user/user").then((res) =>
+      res.json().then((data) => {
+        // console.log(data.markets);
+        setCoins(data.coins);
+      })
+    );
+  }, []);
   return (
     <div className="navigation">
       <FaceIcon />
       <a href="/account">My Account</a>
-      <AddBoxIcon /> Coins
+      <AddBoxIcon /> {coins} Coins
       <NotificationsIcon /> <a href="/listing/add">Manage Listing</a>
       <div className="navigation__box">
         <h1>Trending</h1>
